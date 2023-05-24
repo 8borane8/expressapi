877165a61ea36857e07b4028915339873c13cc64
Borane <74107604+8borane8@users.noreply.github.com>
2023-05-24 15:03:17 +0200

    Patch JsonToken

--- src/classes/JsonToken.js
+++ src/classes/JsonToken.js
@@ -9,7 +9,7 @@ class JsonToken{
         let encoded_payload = CryptoJS.enc.Utf8.parse(JSON.stringify(payload));
         encoded_payload = CryptoJS.enc.Base64url.stringify(encoded_payload);
 
-        return `${encoded_payload}.${sha256(encoded_payload + this.secret)}`;
+        return `${encoded_payload}.${CryptoJS.SHA256(encoded_payload + this.secret)}`;
     }
 
     verify(token){
@@ -17,7 +17,7 @@ class JsonToken{
             let decoded_token = CryptoJS.enc.Base64url.parse(token.split(".")[0]);
             decoded_token = JSON.parse(CryptoJS.enc.Utf8.stringify(decoded_token));
 
-            if(sha256(token.split(".")[0] + this.secret) == token.split(".")[1]){
+            if(CryptoJS.SHA256(token.split(".")[0] + this.secret) == token.split(".")[1]){
                 return decoded_token;
             }
         }catch{}
