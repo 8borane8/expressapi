b307409551b830bff0367b96423a1ed7e8289d41
Borane <74107604+8borane8@users.noreply.github.com>
2023-07-18 10:53:51 +0200

    Correction request.

--- src/index.js
+++ src/index.js
@@ -3,30 +3,43 @@ const https = require("https");
 const http = require("http");
 
 
-function request(url, options = null){
+function request(url, options = null) {
     options = options == null ? {
         method: "GET",
         headers: {}
     } : options;
-
-    return new Promise(function(resolve, reject){
-        (url.startsWith("https://") ? https : http).request(url, options, function(response){
+  
+    return new Promise(function (resolve, reject) {
+      const protocol = url.startsWith("https://") ? require("https") : require("http");
+      const req = protocol.request(url, options, function (response) {
             const body = [];
-
-            response.once("error", function(error){
+    
+            response.on("error", function (error) {
                 reject(error);
             });
-
-            response.on("data", function(chunk){
+    
+            response.on("data", function (chunk) {
                 body.push(chunk);
             });
-          
-            response.on("end", function(){
-                resolve(body);
+    
+            response.on("end", function () {
+                const responseText = Buffer.concat(body).toString();
+
+                if (response.headers["content-type"] && response.headers["content-type"].startsWith("application/json")) {
+                    resolve(JSON.parse(responseText));
+                }else{
+                    resolve(responseText);
+                }
             });
-        }).end(options.body);
+        });
+
+        req.on("error", function (error) {
+            reject(error);
+        });
+    
+        req.end(options.body);
     });
-}
+  }
 
 function encodeBody(dic){
     return encodeURI(
